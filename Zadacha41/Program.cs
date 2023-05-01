a0948f2a28314458c4e561ea2804bc7b988b4ebd
AleksandrGrabov <grabov-it@yandex.by>
2023-05-01 22:08:21 +0300

    Решили задачу 41

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите количество чисел:");
+        int m = int.Parse(Console.ReadLine());
+
+        int count = 0; // создаем переменную для хранения количества чисел больше 0
+
+        for (int i = 1; i <= m; i++)
+        {
+            Console.WriteLine($"Введите число {i}:");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                count++; // если число больше 0, увеличиваем счетчик на 1
+            }
+        }
+
+        Console.WriteLine($"Количество чисел больше 0: {count}");
+
+        Console.ReadLine();
